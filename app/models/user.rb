3039f17cc78f3439cbc437fcabbd1361eef5a607
taijusugahara <96ayutuki@gmail.com>
2021-03-30 16:07:56 +0900

    テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: {with:/[^ -~｡-ﾟ]+/}
+  validates :first_name, presence: true, format: {with:/[^ -~｡-ﾟ]+/}
+  validates :last_name_kana, presence: true, format: {with:/[\p{katakana}　ー－]+/}
+  validates :first_name_kana, presence: true, format: {with:/[\p{katakana}　ー－]+/}
   validates :birthday, presence: true
+  validates :encrypted_password,:password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
 
 end
