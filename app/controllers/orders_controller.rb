41b3c564f029b372f30568ac83b3a8f14e2452d8
taijusugahara <96ayutuki@gmail.com>
2021-04-02 23:45:52 +0900

    Formオブジェクトパターン

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,22 @@
+class OrdersController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end  
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+       @order_address.save
+       redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private 
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :town, :residence, :building, :tell_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
+end
---
+++ app/models/order_address.rb
@@ -0,0 +1,18 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :town, :residence, :building, :tell_number, :order_id
+
+  with_options presence: true do
+    validates :user_id, :item_id, :town, :residence
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :prefecture_id, numericality: {other_than: 1}
+    validates :tell_number,  format: {with: /\A\d{10,11}\z/}
+
+
+  end
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, town: town, residence: residence, building: building, tell_number: tell_number, order_id: order.id)
+  end
+
+end
