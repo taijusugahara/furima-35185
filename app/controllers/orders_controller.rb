5d71f7907edc0194efd3810efcec04f679c9e795
taijusugahara <96ayutuki@gmail.com>
2021-04-03 12:22:17 +0900

    クレジットカード

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
        @order_address.save
        redirect_to root_path
     else
@@ -17,6 +18,15 @@ class OrdersController < ApplicationController
 
   private 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :town, :residence, :building, :tell_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :town, :residence, :building, :tell_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],    
+      currency: 'jpy'                 
+    )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,9 +1,9 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :town, :residence, :building, :tell_number, :order_id
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :town, :residence, :building, :tell_number, :order_id, :token
 
   with_options presence: true do
-    validates :user_id, :item_id, :town, :residence
+    validates :user_id, :item_id, :town, :residence, :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :prefecture_id, numericality: {other_than: 1}
     validates :tell_number,  format: {with: /\A\d{10,11}\z/}
