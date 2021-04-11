1017d53d378ef7c906736534e62a6b3ed62e94e9
taijusugahara <96ayutuki@gmail.com>
2021-04-11 18:46:15 +0900

    複数画像機能

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,33 +1,37 @@
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
 function preview (){
   const ImageList = document.getElementById('image-list');
-  
-  const createImageHTML = (abc) => {
-  const DivElement = document.createElement('div');
-  const ImageElement = document.createElement('img');
-  ImageElement.setAttribute('src', url);
-
-  DivElement.appendChild(ImageElement);
-  ImageList.appendChild(DivElement);
-  };
-  document.getElementById("item-image").addEventListener('change', function(e){
-    const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
-      }
-    const file = e.target.files[0];
-    const url = window.URL.createObjectURL(file);
-
-    // 画像を表示するためのdiv要素を生成
+  console.log(ImageList)
+  const createImageHTML = (url) => {
     const DivElement = document.createElement('div');
-
-    // 表示する画像を生成
+    DivElement.setAttribute('class', "image-element");
+    let imageElementNum = document.querySelectorAll('.image-element').length;
     const ImageElement = document.createElement('img');
     ImageElement.setAttribute('src', url);
 
+    const inputHTML = document.createElement('input');
+    inputHTML.setAttribute('id', `item_image_${imageElementNum}`);
+    inputHTML.setAttribute('name', 'item[images][]');
+    inputHTML.setAttribute('type', 'file');
+
     DivElement.appendChild(ImageElement);
+    DivElement.appendChild(inputHTML);
     ImageList.appendChild(DivElement);
-    createImageHTML(abc);
+
+    inputHTML.addEventListener('change', (e) => {
+      file = e.target.files[0];
+      url = window.URL.createObjectURL(file);
+
+      createImageHTML(url)
+    })
+  };
+  document.getElementById("item-image").addEventListener('change', function(e){
+   
+    const file = e.target.files[0];
+    const url = window.URL.createObjectURL(file);
+
+   
+    createImageHTML(url);
   });
 
   
